94bad9ae27dfa8b68973345be9a95bce20731d6c
IvanGBdemo <82132183+IvanGBdemo@users.noreply.github.com>
2021-08-29 17:55:32 +0300

    Added quickstarts project

---
+++ src/test/java/ru/geekbrains/IntegrationTest.java
@@ -0,0 +1,18 @@
+package ru.geekbrains;
+
+import org.junit.Test;
+
+public class IntegrationTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("integration test1");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2() {
+        System.out.println("integration test2");
+    }
+}
